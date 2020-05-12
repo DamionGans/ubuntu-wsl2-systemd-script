5b81d71022b8449091ec135cd42fa6e9994f0809
Damion Gans <damiongans@gmail.com>
2020-05-12 12:14:48 +0200

    Revert "Update scripts for ZSH and Ubuntu 20.04 compatibility"

--- ubuntu-wsl2-systemd-script.sh
+++ ubuntu-wsl2-systemd-script.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
-
-if [ -f /usr/sbin/start-systemd-namespace ] && [ "$1" != "--force" ]; then
-  echo "It appears you have already installed the systemd hack."
-  echo "To forcibly reinstall, run this script with the \`--force\` parameter."
+if [[ "$EUID" -ne 0 ]]; then 
+  echo "Please run as root/sudo"
   exit
 fi
-
-self_dir="$(dirname $0)"
-
-sudo apt-get update && sudo apt-get install -yqq daemonize dbus-user-session fontconfig
-
-sudo cp "$self_dir/start-systemd-namespace" /usr/sbin/start-systemd-namespace
-sudo cp "$self_dir/enter-systemd-namespace" /usr/sbin/enter-systemd-namespace
-sudo chmod +x /usr/sbin/enter-systemd-namespace
-
-sudo tee /etc/sudoers.d/systemd-namespace <<EOF
-Defaults        env_keep += WSLPATH
-Defaults        env_keep += WSLENV
-Defaults        env_keep += WSL_INTEROP
-Defaults        env_keep += WSL_DISTRO_NAME
-Defaults        env_keep += PRE_NAMESPACE_PATH
-%sudo ALL=(ALL) NOPASSWD: /usr/sbin/enter-systemd-namespace
-EOF
-
+apt-get update && sudo apt-get install -yqq daemonize dbus-user-session fontconfig
+cp ./start-systemd-namespace /usr/sbin/.
+cp ./enter-systemd-namespace /usr/sbin/.
+chmod +x /usr/sbin/enter-systemd-namespace
+echo 'Defaults        env_keep += WSLPATH' | EDITOR='tee -a' visudo
+echo 'Defaults        env_keep += WSLENV' | EDITOR='tee -a' visudo
+echo 'Defaults        env_keep += WSL_INTEROP' | EDITOR='tee -a' visudo
+echo 'Defaults        env_keep += WSL_DISTRO_NAME' | EDITOR='tee -a' visudo
+echo 'Defaults        env_keep += PRE_NAMESPACE_PATH' | EDITOR='tee -a' visudo
+echo '%sudo ALL=(ALL) NOPASSWD: /usr/sbin/enter-systemd-namespace' | EDITOR='tee -a' visudo
 sudo sed -i 2a"# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/bash.bashrc
-
-if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ] && [ "$(head -n1  /proc/sys/fs/binfmt_misc/WSLInterop)" == "enabled" ]; then
-  cmd.exe /C setx WSLENV BASH_ENV/u
-  cmd.exe /C setx BASH_ENV /etc/bash.bashrc
-fi
