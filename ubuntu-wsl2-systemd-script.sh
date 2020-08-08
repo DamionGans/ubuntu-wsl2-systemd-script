e4ecb3cb2e55125ac8aa4462e10e0ba8e8a5d227
Daniel Kaminski de Souza <51767368+DanielAtKrypton@users.noreply.github.com>
2020-08-08 04:34:01 +0100

    Update ubuntu-wsl2-systemd-script.sh
    Best to use sudo command outside of script.

--- ubuntu-wsl2-systemd-script.sh
+++ ubuntu-wsl2-systemd-script.sh
@@ -44,32 +44,32 @@ function sysdrive_prefix {
 	unset hard_reset
 }
 
-sudo hwclock -s
-sudo apt-get update && sudo apt-get install -yqq daemonize dbus-user-session fontconfig
+hwclock -s
+apt-get update && apt-get install -yqq daemonize dbus-user-session fontconfig
 
-sudo cp "$self_dir/start-systemd-namespace" /usr/sbin/start-systemd-namespace
-sudo cp "$self_dir/enter-systemd-namespace" /usr/sbin/enter-systemd-namespace
-sudo chmod +x /usr/sbin/enter-systemd-namespace
+cp "$self_dir/start-systemd-namespace" /usr/sbin/start-systemd-namespace
+cp "$self_dir/enter-systemd-namespace" /usr/sbin/enter-systemd-namespace
+chmod +x /usr/sbin/enter-systemd-namespace
 
-sudo tee /etc/sudoers.d/systemd-namespace >/dev/null <<EOF
+tee /etc/sudoers.d/systemd-namespace >/dev/null <<EOF
 Defaults        env_keep += WSLPATH
 Defaults        env_keep += WSLENV
 Defaults        env_keep += WSL_INTEROP
 Defaults        env_keep += WSL_DISTRO_NAME
 Defaults        env_keep += PRE_NAMESPACE_PATH
 Defaults        env_keep += PRE_NAMESPACE_PWD
-%sudo ALL=(ALL) NOPASSWD: /usr/sbin/enter-systemd-namespace
+%ALL=(ALL) NOPASSWD: /usr/sbin/enter-systemd-namespace
 EOF
 
 if ! grep 'start-systemd-namespace' /etc/bash.bashrc >/dev/null; then
-  sudo sed -i 2a"# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/bash.bashrc
+  sed -i 2a"# Start or enter a PID namespace in WSL2\nsource /usr/sbin/start-systemd-namespace\n" /etc/bash.bashrc
 fi
 
-sudo rm -f /etc/systemd/user/sockets.target.wants/dirmngr.socket
-sudo rm -f /etc/systemd/user/sockets.target.wants/gpg-agent*.socket
-sudo rm -f /lib/systemd/system/sysinit.target.wants/proc-sys-fs-binfmt_misc.automount
-sudo rm -f /lib/systemd/system/sysinit.target.wants/proc-sys-fs-binfmt_misc.mount
-sudo rm -f /lib/systemd/system/sysinit.target.wants/systemd-binfmt.service
+rm -f /etc/systemd/user/sockets.target.wants/dirmngr.socket
+rm -f /etc/systemd/user/sockets.target.wants/gpg-agent*.socket
+rm -f /lib/systemd/system/sysinit.target.wants/proc-sys-fs-binfmt_misc.automount
+rm -f /lib/systemd/system/sysinit.target.wants/proc-sys-fs-binfmt_misc.mount
+rm -f /lib/systemd/system/sysinit.target.wants/systemd-binfmt.service
 
 if [ -f /proc/sys/fs/binfmt_misc/WSLInterop ] && [ "$(head -n1  /proc/sys/fs/binfmt_misc/WSLInterop)" == "enabled" ]; then
   "$(interop_prefix)$(sysdrive_prefix)"/Windows/System32/cmd.exe /C setx WSLENV BASH_ENV/u
