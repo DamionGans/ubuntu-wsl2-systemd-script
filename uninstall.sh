defc14aa9ee847cf47b6f09bb9241b4450736076
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2020-10-30 20:04:33 -0400

    Added [sudo] check

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+  exec sudo "$0" "$1"
+fi
+
 #sudo apt update -yqqqq
 echo "Uninstalling ubuntu-wsl2-systemd-hack"
 sudo rm -f /usr/sbin/start-systemd-namespace
