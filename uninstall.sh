286e8caf3a5dbe54cb67556025fccf08429d5c8a
kitty-kat <54408289+wackyblackie@users.noreply.github.com>
2020-11-23 11:00:40 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,11 @@ if [ "$(id -u)" != "0" ]; then
   exec sudo "$0" "$1"
 fi
 
+cd2=$(pwd)
 #sudo apt update -yqqqq
 echo "Uninstalling ubuntu-wsl2-systemd-hack"
 sudo rm -f /usr/sbin/start-systemd-namespace
 sudo rm -f /usr/sbin/enter-systemd-namespace
-sudo sed -i "s/#\ Start\ or\ enter\ a\ PID\ namespace\ in\ WSL2\nsource\ \/usr\/sbin\/start-systemd-namespace\n//g" /etc/bash.bashrc
+cd /var/tmp
+sudo grep -v "source /usr/sbin/start-systemd-namespace" /etc/bash.bashrc > tmpfile && mv tmpfile /etc/bash.bashrc
+cd $cd2
